b10049ffe58ee86e9c9ce554227f06cb68f032c5
munimrahman <munimrh@gmail.com>
2023-02-07 18:51:42 +0600

    hero section

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import banner1 from '../../../assets/banner1.png';
+import banner2 from '../../../assets/banner2.png';
 import heroBg from '../../../assets/hero-bg.svg';
 import Navbar from '../../Shared/Navbar/Navbar';
+import './Hero.css';
+
+const styleObj = {
+    backgroundImage: `url(${heroBg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    minHeight: '750px',
+    width: '100%',
+};
 
 function Hero() {
     const [stickyNav, setStickyNav] = useState(false);
@@ -13,7 +24,7 @@ function Hero() {
         };
     }, [setStickyNav]);
     return (
-        <div style={{ backgroundImage: `url(${heroBg})` }}>
+        <div style={styleObj}>
             <Navbar
                 stickyNav={
                     stickyNav
@@ -21,9 +32,46 @@ function Hero() {
                         : 'py-6'
                 }
             />
-            <div className="max-w-[1115px] mx-auto">
-                <h1 className="my-10 border border-red-500">Hero Section</h1>
-                <h1 className="my-10">Hero Section</h1>
+            <div className="max-w-[1115px] mx-auto mt-12">
+                <div className="grid grid-cols-2">
+                    <div className="px-14">
+                        <h1 className="text-5xl text-secondary font-bold">
+                            The <span className="text-primary">Easiest Way</span> to Get Your New
+                            Job
+                        </h1>
+                        <p className="text-accent text-lg mt-6">
+                            Each month, more than 3 million job seekers turn to website in their
+                            search for work, making over 140,000 applications every single day
+                        </p>
+                        <div className="form-control w-full max-w-xs mt-6">
+                            <input
+                                type="text"
+                                placeholder="Type here"
+                                className="input input-bordered w-full max-w-xs"
+                            />
+                        </div>
+                        <p className="mt-6 text-md text-accent">
+                            <span className="font-bold">Popular Searches:</span>{' '}
+                            <span className="underline underline-offset-2">Designer,</span>{' '}
+                            <span className="underline underline-offset-2">Web,</span>{' '}
+                            <span className="underline underline-offset-2">IOS,</span>{' '}
+                            <span className="underline underline-offset-2">Developer,</span>{' '}
+                            <span className="underline underline-offset-2">React,</span>{' '}
+                            <span className="underline underline-offset-2">Java,</span>{' '}
+                            <span className="underline underline-offset-2">Python,</span>{' '}
+                            <span className="underline underline-offset-2">JavaScript,</span>{' '}
+                            <span className="underline underline-offset-2">PHP,</span>{' '}
+                        </p>
+                    </div>
+                    <div className="mt-3">
+                        <div className="flex justify-center object">
+                            <img src={banner1} alt="" />
+                        </div>
+                        <div className="mt-12 object1">
+                            <img src={banner2} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Footer from '../../Shared/Footer/Footer';
 import Hero from '../Hero/Hero';
 
@@ -6,7 +7,10 @@ function Home() {
     return (
         <section>
             <Hero />
-            <h1 className="text-3xl text-secondary text-center">This is Home Page</h1>
+            <h1 className="text-4xl text-secondary text-center font-bold">Browse by Category</h1>
+            <p className="text-lg text-neutral text-center">
+                Find the job that’s perfect for you. about 800+ new jobs everyday
+            </p>
             <div className="flex flex-col">
                 <div className="text-primary text-5xl p-10 m-5">
                     <p>Hello Home Page!</p>
