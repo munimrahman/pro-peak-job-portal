9d1e7bc8f2b8bae5dddf1c6c15c49def06274f19
munimrahman <munimrh@gmail.com>
2023-02-08 16:13:12 +0600

    hiring box responsive fixed

--- src/Pages/Home/WeHiringBox/WeHiringBox.jsx
+++ src/Pages/Home/WeHiringBox/WeHiringBox.jsx
@@ -4,11 +4,11 @@ import './WehiringBox.css';
 
 function WeHiringBox() {
     return (
-        <div className="flex justify-center border rounded-md  max-w-[950px] mx-auto mt-14 hiring-box px-5 py-7 mb-5">
-            <h4 className="text-neutral font-bold text-xl self-start">
+        <div className="flex flex-col items-center sm:flex-row justify-around md:justify-center border rounded-md  max-w-[950px] mx-auto mt-14 hiring-box px-5 py-7 mb-5">
+            <h4 className="text-neutral font-bold text-xl md:self-start">
                 WE ARE <br /> <span className="text-5xl text-secondary">HIRING</span>
             </h4>
-            <p className="text-neutral text-lg md:mt-8 md:mx-5">
+            <p className="text-neutral text-2xl md:text-lg my-4 md:my-0 md:mt-8 md:mx-5">
                 Let’s <span className="text-secondary font-bold">Work</span> <br /> Together
                 <br /> & <span className="text-secondary font-bold">Explore</span> <br />{' '}
                 Opportunities
