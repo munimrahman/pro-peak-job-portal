275da7cfe1443f1bb0ab18e28c0e235b2ee0d5b3
munimrahman <munimrh@gmail.com>
2023-02-22 21:48:44 +0600

    search box responsive issue fixd

--- src/Pages/Shared/HeaderBrowseSearch/HeaderBrowseSearch.jsx
+++ src/Pages/Shared/HeaderBrowseSearch/HeaderBrowseSearch.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import key from '../../../assets/key-search-bar.svg';
+// import key from '../../../assets/key-search-bar.svg';
 
 function HeaderBrowseSearch({ title, description }) {
     return (
@@ -7,12 +7,12 @@ function HeaderBrowseSearch({ title, description }) {
             <h1 className="text-secondary text-3xl font-bold">{title}</h1>
             <p className="md:w-6/12 text-center text-sm text-neutral mt-3 mb-10">{description}</p>
             <div className="relative">
-                <div className="absolute top-[23px] left-3">
-                    <img src={key} alt="" />
+                <div className="hidden md:block absolute top-4 left-[14px]">
+                    <i className="fas fa-search text-neutral" />
                 </div>
                 <input
                     type="text"
-                    className="h-14 pl-10 pr-20 rounded-xl z-0 focus:outline-none"
+                    className="h-14 pl-5 md:pl-10 md:pr-20 rounded-xl z-0 focus:outline-none"
                     placeholder="Search Name..."
                 />
                 <div className="absolute top-2 right-2">
--- src/Pages/Shared/SubscribeBox/SubscribeBox.jsx
+++ src/Pages/Shared/SubscribeBox/SubscribeBox.jsx
@@ -14,15 +14,26 @@ function SubscribeBox() {
             className="p-14 mt-16 rounded-2xl border flex justify-center"
         >
             <img src={imgLeft} alt="" className="hidden lg:block" />
-            <div className="text-center self-center">
-                <h1 className="text-2xl sm:text-4xl font-serif font-bold text-white mb-7 w-10/12 mx-auto">
+            {/* <div className="text-center self-center"> */}
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl sm:text-4xl font-serif font-bold text-white mb-7 w-10/12 mx-auto text-center">
                     New Things Will Always Update Regularly
                 </h1>
-                <input
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered w-full max-w-xs"
-                />
+                <div className="relative">
+                    <div className="hidden md:block absolute top-4 left-[14px]">
+                        <i className="fas fa-envelope text-neutral" />
+                    </div>
+                    <input
+                        type="text"
+                        className="h-14 pl-5 md:pl-10 md:pr-20 rounded-xl z-0 focus:outline-none"
+                        placeholder="Your Email"
+                    />
+                    <div className="absolute top-2 right-2">
+                        <button className="flex justify-center items-center h-10 w-20 text-white rounded-xl bg-primary hover:bg-secondary">
+                            Search
+                        </button>
+                    </div>
+                </div>
             </div>
             <img src={imgRight} alt="" className="hidden lg:block" />
         </div>
